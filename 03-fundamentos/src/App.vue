8de52d11d3cee3941096d69167dfeef8cc5a594f
Alexander Franco <85953377+AlexandeerFG@users.noreply.github.com>
2024-01-24 18:01:35 -0600

    termine aplicacion con VUE_CLI

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-
-  <Counter title="Entraron" :start="1"/>
-
-
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <Counter title="Entraron" :start="1" /> -->
+  <Indecision />
 </template>
 
 <script>
-import Counter from './components/Counter.vue'
+// import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'app',
-  components: { 
-    Counter 
+  components: {
+    // Counter,
+    Indecision
   },
 
 }
